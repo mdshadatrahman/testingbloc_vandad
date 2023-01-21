b0b8aa948d2e7a7912f81f57fde62db86ba6ea40
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-21 17:20:38 +0600

    login button

---
+++ lib/views/login_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:testing_bloc_vandad/dialogs/generic_dialog.dart';
+import 'package:testing_bloc_vandad/strings.dart';
+
+typedef OnLoginTapped = void Function(String email, String password);
+
+class LoginButton extends StatelessWidget {
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final OnLoginTapped onLoginTapped;
+  const LoginButton({
+    super.key,
+    required this.emailController,
+    required this.passwordController,
+    required this.onLoginTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        final email = emailController.text;
+        final password = passwordController.text;
+        if (email.isEmpty || password.isEmpty) {
+          showGenericDialog(
+            context: context,
+            title: emailOrPasswordEmptyDialog,
+            content: emailOrPasswordEmptyDescription,
+            optionsBuilder: () => {ok: true},
+          );
+        } else {
+          onLoginTapped(email, password);
+        }
+      },
+      child: const Text(login),
+    );
+  }
+}
