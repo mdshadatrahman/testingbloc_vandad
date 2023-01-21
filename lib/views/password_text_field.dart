669fe30710740be519cb747a0df0f90fc72b1af2
shadat rahman <shadat.rahman.464@gmail.com>
2023-01-21 17:02:01 +0600

    passwordTextField

---
+++ lib/views/password_text_field.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:testing_bloc_vandad/strings.dart' show enterYourPasswordHere;
+
+class PasswordTextField extends StatelessWidget {
+  final TextEditingController passwordController;
+  const PasswordTextField({
+    super.key,
+    required this.passwordController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: passwordController,
+      obscureText: true,
+      obscuringCharacter: '🔘',
+      decoration: const InputDecoration(
+        hintText: enterYourPasswordHere,
+      ),
+    );
+  }
+}
